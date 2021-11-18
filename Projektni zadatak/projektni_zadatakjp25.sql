d88ffd909a868f4f8695c9b6ed6ae24d35adbe47
mariokordic <mario.kordic4@gmail.com>
2021-11-18 21:57:34 +0100

    Dodan korisnik

--- Projektni zadatak/projektni_zadatakjp25.sql
+++ Projektni zadatak/projektni_zadatakjp25.sql
@@ -11,3 +11,19 @@ CREATE table djelatnik(
     drzava varchar(20),
     postanski_broj varchar(10)
 );
+
+
+CREATE table korisnik(
+
+    sifra int,
+    ime varchar(50),
+    prezime varchar(50),
+    broj_mobitela varchar(30),
+    adresa varchar(50),
+    grad varchar (50),
+    drzava varchar(20),
+    postanski_broj varchar(10)
+    spol char(1),
+    usluga int,
+    termin int,
+);
