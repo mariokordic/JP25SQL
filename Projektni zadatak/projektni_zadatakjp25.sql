c185e5336fa3ee19e66844008a84b37e2ee066e5
mariokordic <mario.kordic4@gmail.com>
2021-11-22 09:39:43 +0100

    Dodan korisnik i usluga

--- Projektni zadatak/projektni_zadatakjp25.sql
+++ Projektni zadatak/projektni_zadatakjp25.sql
@@ -63,7 +63,9 @@ values (null,'Mirela','Čulić');
 
 #1
 insert  into korisnik (sifra,ime,prezime,broj_mobitela,adresa,grad,drzava,postanski_broj,spol)
-values (null,'Marija','Magusić','0981767036','Ilirska 56','Osijek','Hrvatska','31000','Ž');
+values (null,'Marija','Magusić','0981767036','Ilirska 56','Osijek','Hrvatska','31000','Ž'),
+        (null,'Mario','Kordić','0997078094','Ilirska 56','Osijek','Hrvatska','31000','M');
+;
 #1
 insert  into usluga (sifra,naziv,trajanje,djelatnik,korisnik,cijena)
 values (null,'Feniranje','30',1,1,50);
@@ -73,3 +75,6 @@ values (null,1,1,'2021-11-22 20:30:00');
 #1
 insert into isporucena_usluga (sifra,usluga,djelatnik,termin,ukupna_cijena)
 values (null, 1,1,1,50);
+#2
+insert  into usluga (sifra,naziv,trajanje,djelatnik,korisnik,cijena)
+values (null,'Šišanje','35',1,2,35);
